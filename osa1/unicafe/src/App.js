d4f29dd1a6f7d50e55ab55d974362d3984a4ba91
cmhhelgeson <62450112+cmhhelgeson@users.noreply.github.com>
2021-10-27 22:25:17 -0700

    Changed the code to reflect an updated understanding of how components accept functions and how to prevent stale states

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, {useState} from 'react'
+
+const Header = () => {
+  return <h1>Give Feedback</h1>
+}
+
+
+const App = () => {
+
+  const [state, updateState] = useState({
+    good: 0,
+    neutral: 0, 
+    bad: 0
+  });
+
+  const updateKey = (key_name) => {
+    updateState(state => (
+      {...state, [key_name]: state[key_name] + 1})
+    );
+  }
+  
+  //onClick={() => updateKey('good')} this format makes it explicit that
+  //you are passing in a function signature rather than a return value
+  //onClick={updateKey('good')} calls that function on every render, not every button press, 
+  //which causes the program to crash
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Header />
+    <button onClick={() => updateKey('good')}>Good</button>
+    <button onClick={() => updateKey('bad')}>Bad</button>
+    <button onClick={() => updateKey('neutral')}>Neutral</button>
+    <p>Good: {state.good}   Bad: {state.bad}   Neutral: {state.neutral}</p>
     </div>
+
+  
   );
 }
 
